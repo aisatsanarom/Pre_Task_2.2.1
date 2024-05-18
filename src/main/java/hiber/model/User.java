0619a2a018439487ae0c16e0b6e9db92d733c205
isatsanarom <morozova18080108@gmail.com>
2024-05-18 03:17:02 +0300

    commit1

--- src/main/java/hiber/model/User.java
+++ src/main/java/hiber/model/User.java
@@ -1,6 +1,7 @@
 package hiber.model;
 
 import javax.persistence.*;
+import java.util.List;
 
 @Entity
 @Table(name = "users")
@@ -18,6 +19,10 @@ public class User {
 
    @Column(name = "email")
    private String email;
+
+   @OneToOne (cascade = CascadeType.ALL)
+   @JoinColumn (name="car_id", referencedColumnName = "id")
+   private Car car;
 
    public User() {}
    
@@ -57,5 +62,13 @@ public class User {
 
    public void setEmail(String email) {
       this.email = email;
+   }
+
+   public Car getCar() {
+      return car;
+   }
+
+   public void setCar(Car car) {
+      this.car = car;
    }
 }
