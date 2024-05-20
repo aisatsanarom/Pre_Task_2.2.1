bd01644c262d49377c70fb84dbcc67b8e2625a8b
isatsanarom <morozova18080108@gmail.com>
2024-05-20 18:59:36 +0300

    commit5

--- src/main/java/hiber/dao/UserDaoImp.java
+++ src/main/java/hiber/dao/UserDaoImp.java
@@ -29,9 +29,14 @@ public class UserDaoImp implements UserDao {
    }
    @Override
    public void getUserByCarModelAndSeries(String model, int series){
-      User user = sessionFactory.getCurrentSession().createQuery("FROM User as u where u.car.model=:carModel and  u.car.series=:carSeries", User.class)
-              .setParameter("carModel", model).setParameter("carSeries", series).uniqueResult();
-      System.out.println(user);
+//      User user = sessionFactory.getCurrentSession().createQuery("FROM User as u WHERE u.car.model=:carModel AND u.car.series=:carSeries", User.class)
+//              .setParameter("carModel", model).setParameter("carSeries", series).uniqueResult();
+//      System.out.println(user);
+
+      TypedQuery<Car> query = sessionFactory.getCurrentSession().createQuery("FROM Car as с WHERE model=:carModel AND series=:carSeries", Car.class)
+              .setParameter("carModel", model).setParameter("carSeries", series);
+      Car car =query.getSingleResult();
+      System.out.println(car.getUser());
    }
 
 
--- src/main/java/hiber/model/Car.java
+++ src/main/java/hiber/model/Car.java
@@ -13,8 +13,8 @@ public class Car {
     @Column(name = "series")
    private int series;
 
-//@OneToOne (mappedBy = "car")
-//    private User user;
+@OneToOne (mappedBy = "car")
+    private User user;
 
     public Car() {
     }
@@ -47,13 +47,13 @@ public class Car {
         this.series = series;
     }
 
-//    public User getUser() {
-//        return user;
-//    }
-//
-//    public void setUser(User user) {
-//        this.user = user;
-//    }
+    public User getUser() {
+        return user;
+    }
+
+    public void setUser(User user) {
+        this.user = user;
+    }
 
     @Override
     public String toString() {
